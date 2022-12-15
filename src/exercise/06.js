a2973cae07b8ebb4dbe9f4c2b50ce596324ea010
FacundoECruz <facundo.emanuel.cruz.94@gmail.com>
2022-12-15 17:13:42 -0300

    solved onSubmit function exercise

--- src/exercise/06.js
+++ src/exercise/06.js
@@ -3,7 +3,7 @@
 
 import * as React from 'react'
 
-function UsernameForm({onSubmitUsername}) {
+function UsernameForm({event, onSubmitUsername}) {
   // 🐨 add a submit event handler here (`handleSubmit`).
   // 💰 Make sure to accept the `event` as an argument and call
   // `event.preventDefault()` to prevent the default behavior of form submit
@@ -19,11 +19,17 @@ function UsernameForm({onSubmitUsername}) {
 
   // 🐨 make sure to associate the label to the input.
   // to do so, set the value of 'htmlFor' prop of the label to the id of input
+  function handleSubmit(event) {
+    event.preventDefault()
+    const username = event.target[0].value
+    onSubmitUsername(username)
+  }
+
   return (
-    <form>
+    <form onSubmit={handleSubmit}>
       <div>
-        <label>Username:</label>
-        <input type="text" />
+        <label htmlFor="input">Username:</label>
+        <input type="text" id="input" />
       </div>
       <button type="submit">Submit</button>
     </form>
