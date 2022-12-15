5b31d822c53becf23e4ddb22e02016695ec5cc63
FacundoECruz <facundo.emanuel.cruz.94@gmail.com>
2022-12-15 17:54:09 -0300

    handle lowercase input with hook

--- src/exercise/06.js
+++ src/exercise/06.js
@@ -19,19 +19,28 @@ function UsernameForm({event, onSubmitUsername}) {
 
   // 🐨 make sure to associate the label to the input.
   // to do so, set the value of 'htmlFor' prop of the label to the id of input
+  const [error, setError] = React.useState(null)
+
   function handleSubmit(event) {
     event.preventDefault()
     const username = event.target.elements.usernameInput.value
     onSubmitUsername(username)
   }
 
+  function handleChange(event) {
+    const {value} = event.target
+    const isLowerCase = value === value.toLowerCase()
+    setError(isLowerCase ? null : 'Username must be lowercase')
+  }
+
   return (
     <form onSubmit={handleSubmit}>
       <div>
         <label htmlFor="usernameInput">Username:</label>
-        <input type="text" id="usernameInput" />
+        <input type="text" id="usernameInput" onChange={handleChange}/>
       </div>
-      <button type="submit">Submit</button>
+      <div style={{color: 'red'}}>{error}</div>
+      <button disabled={Boolean(error)} type="submit">Submit</button>
     </form>
   )
 }
